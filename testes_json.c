f2e8f00cec2fd2062b63ae84e3b87039859ae281
Tiago Shibata <tiago.shibata@gmail.com>
2014-04-20 00:17:37 -0300

    Nova interface JSON para o código
    Interface com "parse" para gerar estruturas ordenadas com todos os pares recebidos e "get" para procurar um par
    Apenas strings suportadas por enquanto e não há checagem de limites do objeto recebido (gera falha de segmentação se for incompleto)

---
+++ testes_json.c
@@ -0,0 +1,33 @@
+/* Use gcc -Wall -Wextra json2.h json2.c testes_json2.c -D_GNU_SOURCE -g para compilar.
+ * Para testar: digite um objeto JSON válido (apenas Strings são suportadas por enquanto), como:
+ { "nome" : "Carona Comunitária USP", "categoria" : "Projeto de Graduação", "disciplina" : "PCS-3100"}
+ * e depois digite o nome de um par para procurar seu valor
+ */
+
+#include <stdio.h>
+#include "json2.h"
+
+int main () {
+	char str[200];
+	json_parser json;
+	json_pair pairs[200];
+	json.start = str;
+	json.pairs = pairs;
+	
+	fgets(str, 200, stdin);
+	printf("Scanning %s\n", str);
+	json_init(&json);
+
+	if (json_all_parse(&json) < 0)
+		return 1;
+	
+	
+	for (;;) {
+		scanf("%s", str);
+		char *match;
+		if ((match = json_get_str(&json, str)) == NULL)
+			printf("Não encontrado\n");
+		else
+			printf("%s\n", match);
+	}
+}
